621d39b726ea7c2b4cbfa43e52e2c304fdfe6b47
Bruno Sorban <brunosorban@users.noreply.github.com>
2020-11-13 17:54:01 -0300

    Update

--- Canards.py
+++ Canards.py
@@ -11,7 +11,7 @@ df.dropna(inplace = True)
 rho = 1.09
 
 # Desired Parameters
-timeMax = 3
+timeMax = 1
 thetaDotMax = 36 #given in radians per second
 thetaDotDotMax = (0 - thetaDotMax)/timeMax
 
@@ -72,8 +72,10 @@ class Compare:
         self.canard1 = canard1
         self.canard2 = canard2
 
-    def calculate_cnalfa(self, cnalfa0, canard):
-        FD = 2 * np.pi * canard.AR / (cnalfa0 *np.cos(canard.gamac))
+    def calculate_cnalfaT(self, cnalfa0, canard):
+        # Check expressions 2.12 and 2.14
+
+        FD = 2 * np.pi * canard.AR / (cnalfa0 * np.cos(canard.gamac))
         return canard.N/2 * cnalfa0 * FD * (canard.Afins/canard.Aref) * np.cos(canard.gamac) / (2 + FD * ( 1 + (4/FD**2) )**0.5) 
 
     def plot_coeff_curves(self, speed = 0.10, mode = 'lift'):
@@ -86,9 +88,9 @@ class Compare:
         if mode == 'lift':
             # Aplies number of fins to lift coefficient data
             angles1 = [ i/2 for i in range(len(Airfoil1)) ]
-            dataCanard1 = [ self.calculate_cnalfa(float(Airfoil1[i]), self.canard1) for i in range(len(Airfoil1)) ]
+            dataCanard1 = [ self.calculate_cnalfaT(float(Airfoil1[i]), self.canard1) for i in range(len(Airfoil1)) ]
             angles2 = [ i/2 for i in range(len(Airfoil2)) ]
-            dataCanard2 = [ self.calculate_cnalfa(float(Airfoil2[i]), self.canard2) for i in range(len(Airfoil2)) ]
+            dataCanard2 = [ self.calculate_cnalfaT(float(Airfoil2[i]), self.canard2) for i in range(len(Airfoil2)) ]
 
 
             plt.plot(angles1, dataCanard1 , angles2, dataCanard2)
@@ -100,9 +102,9 @@ class Compare:
         if mode == 'forcing':
             # Aplies number of fins to lift coefficient data
             angles1 = [ i/2 for i in range(len(Airfoil1)) ]
-            dataCanard1 = [ self.canard1.YtFins * self.calculate_cnalfa(float(Airfoil1[i]), self.canard1) / (2 * self.canard2.radius) for i in range(len(Airfoil1)) ]
+            dataCanard1 = [ self.canard1.YtFins * self.calculate_cnalfaT(float(Airfoil1[i]), self.canard1) / (2 * self.canard2.radius) for i in range(len(Airfoil1)) ]
             angles2 = [ i/2 for i in range(len(Airfoil2)) ]
-            dataCanard2 = [ self.canard2.YtFins * self.calculate_cnalfa(float(Airfoil2[i]), self.canard2) / (2 * self.canard2.radius) for i in range(len(Airfoil2)) ]
+            dataCanard2 = [ self.canard2.YtFins * self.calculate_cnalfaT(float(Airfoil2[i]), self.canard2) / (2 * self.canard2.radius) for i in range(len(Airfoil2)) ]
             
 
             plt.plot(angles1, dataCanard1 , angles2, dataCanard2)
@@ -113,42 +115,49 @@ class Compare:
 
     def calculateA(self, speed, stall_angle, fin_delta_angle, J, thetaDotDotMax, thetaDot0 = 0, dynamicPressure = 3.105e+04):
         '''The canards set must come before the fins set'''
-        # a condição é só para resolver busg no acesso ao dataset do pandas (M=0.10 e M=0.15, por exemplo)
-        if (10 * speed) % 1 == 0:
-            Airfoil1 = np.array(self.canard1.Cnalfa0["M=" + str(speed) + str(0)])
-            Airfoil2 = np.array(self.canard2.Cnalfa0["M=" + str(speed) + str(0)])
-        else:
-            Airfoil1 = np.array(self.canard1.Cnalfa0["M=" + str(speed)])
-            Airfoil2 = np.array(self.canard2.Cnalfa0["M=" + str(speed)])
+        # condition to find the index at the panda dataset
+        Airfoil1 = np.array(self.canard1.Cnalfa0["M={:.2f}".format(speed)])
+        Airfoil2 = np.array(self.canard2.Cnalfa0["M={:.2f}".format(speed)])
 
-        dataCanard1 = [ [i/2, self.calculate_cnalfa(float(Airfoil1[i]), self.canard1)] for i in range(len(Airfoil1)) ]
-        dataCanard2 = [  [i/2, self.calculate_cnalfa(float(Airfoil2[i]), self.canard2)] for i in range(len(Airfoil2)) ]
+        data0Canard1 = [ [i/2, float(Airfoil1[i])] for i in range(len(Airfoil1)) ]
+        data0Canard2 = [  [i/2, float(Airfoil2[i])] for i in range(len(Airfoil2)) ]
+
+        # Generates the Cnalfa0 as a Function object
+        cn0dataCanard1 = Function(data0Canard1, 'Alpha (rad)', 'Cn', interpolation='linear', extrapolation = 'natural') # Cnalfa1 for each canard set
+        cn0dataCanard2 = Function(data0Canard2, 'Alpha (rad)', 'Cn', interpolation='linear', extrapolation = 'natural')
+
+        # Map the cnalfaT for the 3d fin, taking as base the cnalfa0 of the airfoil and the fin parameters
+        dataCanard1 = [ [i/2, self.calculate_cnalfaT(float(Airfoil1[i]), self.canard1)] for i in range(len(Airfoil1)) ]
+        dataCanard2 = [  [i/2, self.calculate_cnalfaT(float(Airfoil2[i]), self.canard2)] for i in range(len(Airfoil2)) ]
 
         # Converts lift coefficient data to a Function object
         cndataCanard1 = Function(dataCanard1, 'Alpha (rad)', 'Cn', interpolation='linear', extrapolation = 'natural') # Cnalfa1 for each canard set
         cndataCanard2 = Function(dataCanard2, 'Alpha (rad)', 'Cn', interpolation='linear', extrapolation = 'natural')
 
-        ClfCanards = 2 * self.canard1.YtFins * self.calculate_cnalfa(float(Airfoil1[2 * stall_angle]), self.canard1) / (2 * self.canard1.radius) 
+        # Calculate the canards lift coefficient - formula 2.28
+        ClfCanards = 2 * self.canard1.YtFins * self.calculate_cnalfaT(float(Airfoil1[2 * stall_angle]), self.canard1) / (2 * self.canard1.radius) 
         
+        # Calculation of constants for the damping coefficient - formula 2.32
         c1 = ((self.canard1.YtFins) /  2) * (self.canard1.radius**2) * self.canard1.span
         c2 = ((self.canard1.root + 2 * self.canard1.tip) / 3) * self.canard1.radius * (self.canard1.span**2)
         c3 = ((self.canard1.root + 3 * self.canard1.tip) / 12) * (self.canard1.span**3)
         trapezoidal_constant_canards = c1 + c2 + c3
 
         # Está errado, precisa corrigir para a nova fórmula qeu encontramos
-        CldCanards = self.canard1.N * cndataCanard1.differentiate(x = 1e-2, dx = 1e-3) * thetaDot0 * trapezoidal_constant_canards/ (self.canard1.Aref * self.canard1.radius * 2 * speed * 343)
+        CldCanards = self.canard1.N * cn0dataCanard1.differentiate(x = 1e-2, dx = 1e-3) * thetaDot0 * trapezoidal_constant_canards/ (self.canard1.Aref * self.canard1.radius * 2 * speed * 343)
         A = 100 * (ClfCanards + CldCanards)
 
         # Repeat the process to the Amin
-        ClfFins = 2 * self.canard2.YtFins * self.calculate_cnalfa(float(Airfoil2[2 * fin_delta_angle]), self.canard2) / (2 * self.canard2.radius) 
+        ClfFins = 2 * self.canard2.YtFins * self.calculate_cnalfaT(float(Airfoil2[2 * fin_delta_angle]), self.canard2) / (2 * self.canard2.radius) 
         
         b1 = ((self.canard2.YtFins) /  2) * (self.canard2.radius**2) * self.canard2.span
         b2 = ((self.canard2.root + 2 * self.canard2.tip) / 3) * self.canard2.radius * (self.canard2.span**2)
         b3 = ((self.canard2.root + 3 * self.canard2.tip) / 12) * (self.canard2.span**3)
         trapezoidal_constant_fins = b1 + b2 + b3
 
-        CldFins = self.canard2.N * cndataCanard2.differentiate(x = 1e-2, dx = 1e-3) * thetaDot0 * trapezoidal_constant_fins/ (self.canard2.Aref * self.canard2.radius * 2 * speed * 343)
+        CldFins = self.canard2.N * cn0dataCanard2.differentiate(x = 1e-2, dx = 1e-3) * thetaDot0 * trapezoidal_constant_fins/ (self.canard2.Aref * self.canard2.radius * 2 * speed * 343)
         
+
         Amin = 100 * (-J * thetaDotDotMax / (dynamicPressure * self.canard2.Aref * 2 * self.canard2.radius) + ClfFins - CldFins)
 
         print("A = ", A)
@@ -162,4 +171,4 @@ Fin.setCnalfa0(df)
 Comp = Compare(Cana, Fin)
 
 #Comp.plot_coeff_curves(mode='forcing', speed=0.7)
-Comp.calculateA(speed = 0.7, stall_angle= 6, fin_delta_angle= delta, J=J, thetaDotDotMax = 0, thetaDot0=0, dynamicPressure=3.105e+04)
+Comp.calculateA(speed = 0.7, stall_angle= 6, fin_delta_angle= delta, J=J, thetaDotDotMax = thetaDotDotMax, thetaDot0=0, dynamicPressure=3.105e+04)
