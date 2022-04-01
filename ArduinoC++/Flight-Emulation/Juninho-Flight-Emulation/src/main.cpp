b1c4416812be19932912859c1f5cf9074a7668c4
Bruno Sorban <brunosorban@usp.br>
2022-03-31 23:00:34 -0300

    Flight Simmulation Complete

--- ArduinoC++/Flight-Emulation/Juninho-Flight-Emulation/src/main.cpp
+++ ArduinoC++/Flight-Emulation/Juninho-Flight-Emulation/src/main.cpp
@@ -133,6 +133,8 @@ File myFile;
 File myFileSD;
 int flag=0;
 const int chipSelect = BUILTIN_SDCARD;
+char filename[] = "w3_controle_amostrado.csv";
+int recNum = 0; // We have read 0 records so far
 
 // Inicializando variável do Enable
 boolean Enable = true;
@@ -152,45 +154,13 @@ unsigned long micros_anterior = 0;
 
 void setup() {
   //Inicia a comunicaçao serial (para exibir os valores lidos)
-  Serial.begin(115200);
+  Serial.begin(500000);
   while (!Serial) {
     ; // wait for serial port to connect.
   }
 
-  Wire.begin(); //Inicia a comunicação I2C
-  Wire.beginTransmission(MPU_addr); //Começa a transmissao de dados para o sensor
-  Wire.write(0x6B); // registrador PWR_MGMT_1
-  Wire.write(0); // Manda 0 e "acorda" o MPU_addr 6050
-  Wire.endTransmission(true);
-
-  // Configura Giroscópio
-  /*
-    Wire.write(0b00000000); // fundo de escala em +/-250°/s
-    Wire.write(0b00001000); // fundo de escala em +/-500°/s
-    Wire.write(0b00010000); // fundo de escala em +/-1000°/s
-    Wire.write(0b00011000); // fundo de escala em +/-2000°/s
-  */
-
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x1B);
-  Wire.write(0b00011000);  // fundo de escala
-  Wire.endTransmission();
-
-  // Configura Acelerometro
-  /*
-      Wire.write(0b00000000); // fundo de escala em +/-2g
-      Wire.write(0b00001000); // fundo de escala em +/-4g
-      Wire.write(0b00010000); // fundo de escala em +/-8g
-      Wire.write(0b00011000); // fundo de escala em +/-16g
-  */
-
-  Wire.beginTransmission(MPU_addr);
-  Wire.write(0x1C);
-  Wire.write(0b00011000);  // fundo de escala
-  Wire.endTransmission();
-
   // Atribui pino ao servo
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  myservo.attach(10);  // attaches the servo on pin 9 to the servo object
 
   // Prepara controlador
   Setpoint = 0; // Velocidade angular desejada
@@ -198,20 +168,13 @@ void setup() {
   myPID.SetOutputLimits(-8, 8);
   myPID.SetSampleTime(10);
 
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXAccelOffset(-411);
-  mpu.setYAccelOffset(-645);
-  mpu.setZAccelOffset(-1169);
-  mpu.setXGyroOffset(-35);
-  mpu.setYGyroOffset(282);
-  mpu.setZGyroOffset(-17);
-
   Serial.print("Initializing SD card...");
 
   if (!SD.begin(chipSelect)) {
     Serial.println("initialization failed!");
     return;
   }
+  myFileSD = SD.open(filename, FILE_READ);
   Serial.println("initialization done.");
 }
 
@@ -219,39 +182,19 @@ void loop() {
   micros_atual = micros();
   if ((micros_atual - micros_anterior) >= intervalo_de_obtencao_e_filtragem_de_dados ) {
     micros_anterior = micros_atual;
-    Serial.print("micros_atual: ");
-    Serial.println(micros_atual);
-
     inicio = micros(); // <-------------------
-
-    // // Obtem os dados do sensor
-    // Wire.beginTransmission(MPU_addr); //Começa a transmissao de dados para o sensor
-    // Wire.write(0x3B); // registrador dos dados medidos (ACCEL_XOUT_H)
-    // Wire.endTransmission(false);
-    // Wire.requestFrom(MPU_addr, 14, true); // faz um "pedido" para ler 14 registradores, que serão os registrados com os dados medidos
-    // AcX_raw = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-    // AcY_raw = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-    // AcZ_raw = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-    // Tmp_raw = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-    // GyX_raw = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-    // GyY_raw = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-    // GyZ_raw = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-    // AcX = AcX_raw / 2048;
-    // AcY = AcY_raw / 2048;
-    // AcZ = AcZ_raw / 2048;
-    // GyX = GyX_raw / 16.4;
-    // GyY = GyY_raw / 16.4;
-    // GyZ = GyZ_raw / 16.4;
-
+    
   // Faz a leitura do cartao SD
-  if (myFile.available()) { 
-      char letter = myFile.read(); //read next character from file
-      GyZ = 0 * 57.29577951; // converte de radianos para graus
+  if (myFileSD.available()) { 
+      String list = myFileSD.readStringUntil('\n');
+//      Serial.println(list);
+      recNum++; // Count the record
+      GyZ = list.toFloat() * 57.29577951; // converte de radianos para graus
   }
 
   else {
     GyZ = 0;
+    Serial.println("Not Available");
   }
     
   //  float dado_filtrado = filter.addData(GyZ);
@@ -266,44 +209,14 @@ void loop() {
   // Caso o Enable esteja ativado, opera o controle
   else {
     myPID.Compute(); // Atualiza o output do PID
-    myservo.write(Output); // Envia o Output para o servo. Falta a conversão da pushrod
+    myservo.write(Output * 10 + 90); // Envia o Output para o servo. Falta a conversão da pushrod
+//    Serial.print("Output = ");
+//    Serial.println(Output);
   }
-
-    fim = micros(); // <-------------------
-    Serial.print("Tempo lendo dados: "); // <-------------------
-    Serial.println(fim - inicio); // <-------------------
-    inicio = micros(); // <-------------------
-
-    // Salva dados no SD
-    myFileSD = SD.open("TesteBruno.txt", FILE_WRITE);
-
-    // if the file opened okay, write to it:
-    if (myFileSD) {
-      if (flag == 0) {
-        myFileSD.println("acx,acy,acz,tmp,gyx,gyy,gyz,dado_filtrado,tempo");
-        flag = 1;
-      }
-     myFileSD.print(AcX_raw); myFileSD.print(','); myFileSD.print(AcY_raw); myFileSD.print(',');
-     myFileSD.print(AcZ_raw); myFileSD.print(','); myFileSD.print(Tmp); myFileSD.print(',');
-     myFileSD.print(GyX_raw); myFileSD.print(','); myFileSD.print(GyY_raw); myFileSD.print(',');
-     myFileSD.print(GyZ_raw); myFileSD.print(','); myFileSD.print(dado_filtrado);
-      myFileSD.write(','); myFileSD.write(micros_atual + '\n');
-      Serial.println("Aberto E Salvo");
-      // close the file:
-      myFile.close();
-    }
-    else {
-      // if the file didn't open, print an error:
-      Serial.println("error opening TesteBruno.txt");
-    }
-
     Serial.print("GyZ = "); Serial.print(GyZ);
     Serial.print(" | GyZ filtrado = "); Serial.println(dado_filtrado);
-    //  Serial.print(" | Servo Position = "); Serial.println(10 * Output + 90);
+      Serial.print(" | Servo Position = "); Serial.println(10 * Output + 90);
 
     fim = micros(); // <-------------------
-    Serial.print("Tempo gravando dados: "); // <-------------------
-    Serial.println(fim - inicio); // <-------------------
-
   }
 }
