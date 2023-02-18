e6b11d16cafe59b0d795b17281e3bcc737fbdeea
Lucas Wu Jiajun <108297692+wucaslu@users.noreply.github.com>
2023-02-18 01:43:36 -0300

    Update EnableFunction.ino

--- ArduinoC++/Nyx/EnableFunction/EnableFunction.ino
+++ ArduinoC++/Nyx/EnableFunction/EnableFunction.ino
@@ -23,6 +23,11 @@ Adafruit_BMP280 bmp;
 float pressure;
 float altitude;
 
+//Enable
+double minAltitude = 0.0; //dado pelo RocketPy
+double apogeeAltitude = 0.0; //dado pelo RocketPy
+EnableFunction *enable = new EnableFunction(minAltitude, apogeeAltitude);
+
 void setup() {
   Wire.begin();
   
@@ -34,16 +39,12 @@ void setup() {
                 Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
                 Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                 Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
-
-  double minAltitude = 0.0;
-  double apogeeAltitude = 0.0;
-  EnableFunction enable(minAltitude, apogeeAltitude);
 }
 
 void loop() {
 
   mpu1.getMotion6(&ax1_Raw, &ay1_Raw, &az1_Raw, &gx1_Raw, &gy1_Raw, &gz1_Raw);
-  AcX = ax1_Raw / 2048; //ver quais valores sao interessantes converter
+  AcX = ax1_Raw / 2048;
   AcY = ay1_Raw / 2048;
   AcZ = az1_Raw / 2048;
   GyX = gx1_Raw / 16.4;
@@ -62,7 +63,7 @@ void loop() {
   altitude = bmp.readAltitude();
 
   enable->addValues(AcX, altitude); //ver a orientacao do giroscopio
-  if(enable->getControlOn() == true && enable->getApogeeOn() == false) {
+  if(enable->getControlOn() == true && enable->getApogeeAchieved() == false) {
     //PID
   }
 }
