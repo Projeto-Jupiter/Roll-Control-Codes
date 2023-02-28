64519b8b61d2a5be6cba93d11452c954fa20b40f
KrWanderley <81878703+KrWanderley@users.noreply.github.com>
2023-02-28 08:15:59 -0300

    Defining global and setup conditions

--- ArduinoC++/Nyx/ESP32_Code/ESP32_Code.ino
+++ ArduinoC++/Nyx/ESP32_Code/ESP32_Code.ino
@@ -1,208 +1,217 @@
-#include <Wire.h> //Biblioteca para comunicação I2C
-#include <MPU6050.h> // Biblioteca para uso do sensor
-#include <Servo.h>  //Biblioteca para uso dos servos
-#include <PID_v1.h> // Biblioteca com a implementação do controlador PID
-#include <EEPROM.h> //Biblioteca para armazenamento dos dados na memória
-#include <LowPass.h>  //Biblioteca com implementação do filtro passa-baixa
-#include <EnableFunction.h>   //Biblioteca responsável por ligar e desligar os sistemas de controle
+#include <Wire.h>               // Biblioteca para comunicação I2C
+#include <SPIFlash.h>           // Biblioteca para escrita de dados na memória
+#include <SPI.h>                // Biblioteca para seleção HSPI
+#include <MPU6050.h>            // Biblioteca para uso do sensor
+#include <Adafruit_BMP280.h>    // Biblioteca para uso do barômetro
+#include <ESP32Servo.h>         // Biblioteca para uso dos servos
+#include <math.h>               // math.h
+#include "RocketPID.h"          // Biblioteca do PID Implementado pelo Jupiter
+#include <map>                  // Biblioteca para o mapeamento das posições dos servos
+#include "LowPass.h"            // Biblioteca com implementação do filtro passa-baixa
+#include "EnableFunction.h"     // Biblioteca responsável por ligar e desligar os sistemas de controle
 
-// Inicializando variaveis do sensor
-MPU6050 mpu;
-const int MPU_addr = 0x68;                                              //Endereco I2C do sensor MPU_addr-6050
-int16_t AcX_raw, AcY_raw, AcZ_raw, Tmp_raw, GyX_raw, GyY_raw, GyZ_raw;  //Variaveis para pegar os valores medidos
-double AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;                               //Valores finais
+// Inicializando variaveis dos giroscopios
+const int mpuAddr1 = 0x68   // Endereço I2C do primeiro giroscópio (AD0 low)
+const int mpuAddr2 = 0x69   // Endereço I2C do segundo giroscópio (AD0 HIGH)
 
-// inicializa variaveis do servo
-Servo myservo;                                                          // create servo object to control a servo
+MPU6050 mpu1(mpuAddr1);     
+MPU6050 mpu2(mpuAddr2);
+int16_t ax1_Raw, ay1_Raw, az1_Raw, gx1_Raw, gy1_Raw, gz1_Raw;
+int16_t ax2_Raw, ay2_Raw, az2_Raw, gx2_Raw, gy2_Raw, gz2_Raw;
+double AcX1, AcY1, AcZ1, GyX1, GyY1, GyZ1;                               //Valores finais (float?)
+double AcX2, AcY2, AcZ2, GyX2, GyY2, GyZ2;
 
-// Criando o Filtro
+// Inicializando variaveis do barometro
+Adafruit_BMP280 bmp;        //debuggar para encontrar a variavel de 2 bytes
+int16_t altRaw;
+float altitude;
+
+// Variaveis da memoria
+#define HSPI_MISO   12    // pino de saída da memória, entrada do ESP
+#define HSPI_MOSI   13    // pino de entrada da memória, saída do ESP
+#define HSPI_SCLK   14    // pino do clock
+#define HSPI_SS     15    // pino do chipselect
+
+SPIClass *hspi = new SPIClass(HSPI);
+
+int16_t value;            // variável a ser armazenada
+int8_t byte0;             // Byte de value menos significativo
+int8_t byte1;             // Byte de value mais significativo
+uint8_t chipSelect = 15;  // repetir??
+
+SPIFlash flash(chipSelect, hspi);
+
+// inicializa variaveis dos servos
+Servo servo1;
+Servo servo2;
+
+std::map<int, float> myMap = {  // Mapeamento das posições dos servos
+  {-8, 1340}, // -8 -> 1330 - 1348
+  {-7, 1358}, // -7 -> 1349 - 1368
+  {-6, 1378}, // -6 -> 1369 - 1387
+  {-5, 1398}, // -5 -> 1388 - 1407
+  {-4, 1418}, // -4 -> 1408 - 1426
+  {-3, 1438}, // -3 -> 1427 - 1446
+  {-2, 1458}, // -2 -> 1447 - 1465
+  {-1, 1477}, // -1 -> 1466 - 1485
+  {0, 1495},  //  0 -> 1487 - 1502
+  {1, 1513},  //  1 -> 1503 - 1522
+  {2, 1532},  //  2 -> 1523 - 1541 
+  {3, 1552},  //  3 -> 1542 - 1561
+  {4, 1572},  //  4 -> 1562 - 1581
+  {5, 1591},  //  5 -> 1582 - 1600
+  {6, 1612},  //  6 -> 1601 - 1620 
+  {7, 1630},  //  7 -> 1621 - 1639
+  {8, 1650}   //  8 -> 1640 - 1659
+};
+
+// Criando o Filtro ??
 double dado_filtrado;
 LowPass filtro;
 
-// inicializa variaveis do controlador
-double Setpoint, Output, ServoOutput, K_convertion = 1.62;              // Variables of interest
-double Kp = 0.015, Ki = 0.315, Kd = 0;
-PID myPID(&dado_filtrado, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+// Inicializa variaveis do controlador
+float cantAngle;             
+float Kp = 0.005, Ki = 0.01, Kd = 0.000;
+int setPoint = 0;
+RocketPID testePID(setPoint, Kp, Ki, Kd);
+float dado_filtrado;
+float Output;
+float dutyCycle;                          //usado como input do servo, em microssegundos
+int lastCantAngle = 0;                    // em graus
+float lower = -8.0;
+float upper = 8.0;
+float dt = 0.01;
 
-// Enable
-EnableFunction Ena;
-int Enable_eletronica = 0;
-int Enable_nosso = 0;
+// Controle dos estados do foguete
+double minAltitude = 1022.912;                    // Altitude de burnout em (m) (ASL)
+double apogeeAltitude = 2036.743;                 // Apogeu em (m) (ASL)
+EnableFunction *enable = new EnableFunction(minAltitude, apogeeAltitude);
+
+// Variaveis do controle de frequencia
+uint32_t t0, t1, t0mem, t1mem;
+int16_t delta = 0;
+int16_t periodo = 0;
+long memCont = 0;
+
+int dataCont = 0;
 
 // ===========================================================================================================
 void setup() {
   //Inicia a comunicaçao serial (para exibir os valores lidos)
-  Serial.begin(250000);
+  Serial.begin(115200);
 
-  Wire.begin(); //Inicia a comunicação I2C
-  Wire.beginTransmission(MPU_addr); //Começa a transmissao de dados para o sensor
-  Wire.write(0x6B); // registrador PWR_MGMT_1
-  Wire.write(0); // Manda 0 e "acorda" o MPU_addr 6050
-  Wire.endTransmission(true);
+  //Configuração da memória
+  hspi->begin(HSPI_SCLK, HSPI_MISO, HSPI_MOSI, HSPI_SS); //SCLK, MISO, MOSI, SS
+  Wire.begin();
+  flash.begin();
+  //flash.eraseChip(); //Executar uma vez para limpar a memória
 
-  // Configura Giroscópio
-  /*
-    Wire.write(0b00000000); // fundo de escala em +/-250°/s
-    Wire.write(0b00001000); // fundo de escala em +/-500°/s
-    Wire.write(0b00010000); // fundo de escala em +/-1000°/s
-    Wire.write(0b00011000); // fundo de escala em +/-2000°/s
-  */
-  Wire.beginTransmission(MPU_addr);
+  //Configuração dos sensores
+  mpu1.initialize();
+
+  Wire.beginTransmission(0x68);
   Wire.write(0x1B);
   Wire.write(0b00011000);  // fundo de escala
   Wire.endTransmission();
 
-  // Configura Acelerometro
-  /*
-      Wire.write(0b00000000); // fundo de escala em +/-2g
-      Wire.write(0b00001000); // fundo de escala em +/-4g
-      Wire.write(0b00010000); // fundo de escala em +/-8g
-      Wire.write(0b00011000); // fundo de escala em +/-16g
-  */
-  Wire.beginTransmission(MPU_addr);
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1C);
+  Wire.write(0b00011000);  // fundo de escala
+  Wire.endTransmission();
+  
+  mpu1.setXAccelOffset(-2289);  // calibrações
+  mpu1.setYAccelOffset(-616);
+  mpu1.setZAccelOffset(832);
+  mpu1.setXGyroOffset(19);
+  mpu1.setYGyroOffset(31);
+  mpu1.setZGyroOffset(28);
+
+  mpu2.initialize();
+
+  Wire.beginTransmission(0x69);
+  Wire.write(0x1B);
+  Wire.write(0b00011000);  // fundo de escala
+  Wire.endTransmission();
+
+  Wire.beginTransmission(0x69);
   Wire.write(0x1C);
   Wire.write(0b00011000);  // fundo de escala
   Wire.endTransmission();
 
-  // Atribui pino ao servo
-  myservo.attach(3);  // attaches the servo on pin 9 to the servo object
+  mpu2.setXAccelOffset(2146);   // calibrações
+  mpu2.setYAccelOffset(-231);
+  mpu2.setZAccelOffset(1622);
+  mpu2.setXGyroOffset(12);
+  mpu2.setYGyroOffset(1);
+  mpu2.setZGyroOffset(71);
 
-  // Prepara controlador
-  Setpoint = 0; // Velocidade angular desejada
-  myPID.SetMode(AUTOMATIC); //turn the PID on
-  myPID.SetOutputLimits(-8, 8);
-  // myPID.SetSampleTime(10); // The sample time is being manually controlled on the loop
-
-  // supply your own gyro offsets here, scaled for min sensitivity
-  mpu.setXAccelOffset(-411);
-  mpu.setYAccelOffset(-645);
-  mpu.setZAccelOffset(-1169);
-  mpu.setXGyroOffset(-35);
-  mpu.setYGyroOffset(282);
-  mpu.setZGyroOffset(-17);
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode. 
+                  Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling 
+                  Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling 
+                  Adafruit_BMP280::FILTER_X16,      // Filtering. 
+                  Adafruit_BMP280::STANDBY_MS_500); // Standby time. 
   
-  for (pos = 0; pos <= 45; pos += 1) { // goes from 0 degrees to 180 degrees
-    // in steps of 1 degree
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+  //Configuração dos servos
+  servo1.attach(33); 
+  servo2.attach(18);
+
+  //Configuração do controlador
+  setLimits(lower, upper);
+  setDt(dt);
+
+  //"Oi" das cannards
+  servo1.writeMicroseconds(myMap[0]);
+  servo2.writeMicroseconds(myMap[0]);
+  delay(100);
+  for (int pos = 1496; pos <= 2000; pos++){
+    servo1.writeMicroseconds(pos);
+    servo2.writeMicroseconds(pos);
+    delay(2);
   }
-  for (pos = 45; pos >= 0; pos -= 1) { // goes from 45 degrees to 0 degrees
-    myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15 ms for the servo to reach the position
+  delay(1000);
+  for (int pos = 1999; pos >= 1496; pos--){
+    servo1.writeMicroseconds(pos);
+    servo2.writeMicroseconds(pos);
+    delay(2);
   }
+  delay(100);
+  servo1.writeMicroseconds(myMap[0]);
+  servo2.writeMicroseconds(myMap[0]);
+
+  delay(60000);         // delay para inicialização da memória
+  t0 = millis();
 }
-
-
-// ===========================================================================================================
-// Para Debugagem
-unsigned long inicio = 0; // <-------------------
-unsigned long fim = 0; // <-------------------
-
-// Gerenciamento de tempo
-unsigned long intervalo_de_obtencao_e_filtragem_de_dados = 10000; // Em microssegundos
-unsigned long intervalo_de_salvamento_de_dados = 250000; // Em microssegundos
-unsigned long micros_atual = 0;
-unsigned long micros_anterior_obtencao_e_filtragem = 0;
-unsigned long micros_anterior_salvamento_de_dados = 0;
-
-// EEPROM
-int addr = 0;
-int ServoOutputSaved;
-int dadoFiltradoSaved;
 
 void loop() {
 
-  micros_atual = micros();
-  if ((micros_atual - micros_anterior_obtencao_e_filtragem) >= intervalo_de_obtencao_e_filtragem_de_dados ) {
-    micros_anterior_obtencao_e_filtragem = micros_atual;
-//    Serial.print("micros_atual: ");
-//    Serial.println(micros_atual);
+  t1 = micros();
+  if ((t1 - t0) >= 10) {
+    
+    // Obtem os dados dos sensores
+    mpu1.getMotion6(&ax1_Raw, &ay1_Raw, &az1_Raw, &gx1_Raw, &gy1_Raw, &gz1_Raw);
+    mpu2.getMotion6(&ax2_Raw, &ay2_Raw, &az2_Raw, &gx2_Raw, &gy2_Raw, &gz2_Raw);
+    altRaw = ...
+   
+    AcX1 = ax1_Raw / 2048;
+    AcY1 = ay1_Raw / 2048;
+    AcZ1 = az1_Raw / 2048;
+    GyX1 = gx1_Raw / 16.4;
+    GyY1 = gy1_Raw / 16.4;
+    GyZ1 = gz1_Raw / 16.4;
 
-    inicio = micros(); // <-------------------
+    AcX2 = ax2_Raw / 2048;
+    AcY2 = ay2_Raw / 2048;    
+    AcZ2 = az2_Raw / 2048;
+    GyX2 = gx2_Raw / 16.4;    
+    GyY2 = gy2_Raw / 16.4;
+    GyZ2 = gz2_Raw / 16.4;
 
-    // Obtem os dados do sensor
-    Wire.beginTransmission(MPU_addr); //Começa a transmissao de dados para o sensor
-    Wire.write(0x3B); // registrador dos dados medidos (ACCEL_XOUT_H)
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU_addr, 14, true); // faz um "pedido" para ler 14 registradores, que serão os registrados com os dados medidos
-    AcX_raw = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-    AcY_raw = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-    AcZ_raw = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-    Tmp_raw = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-    GyX_raw = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-    GyY_raw = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-    GyZ_raw = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-    AcX = AcX_raw / 2048;
-    AcY = AcY_raw / 2048;
-    AcZ = AcZ_raw / 2048;
-    GyX = GyX_raw / 16.4;
-    GyY = GyY_raw / 16.4;
-    GyZ = GyZ_raw / 16.4;
-
-    //  float dado_filtrado = filter.addData(GyZ);
-    dado_filtrado = filtro.addData(GyZ);
-
-    if (Enable_eletronica == 1 || Enable_nosso == 1) {
-      myPID.Compute();
-
-      // Convert canard output to servo output
-      ServoOutput = K_convertion * Output + 90;
-
-      // Write output on the servp
-      myservo.write(ServoOutput); // Output
+    altitude = bmp.readAltitude();
+    
+    //  float dado_filtrado = filter.addData(GyX);
+    dado_filtrado = filtro.addData(GyX);
+    
+    enable->addValues(,);
+    
     }
-
-    else {
-      myservo.write(90);
-      // Adicionar rotina que verifica o enable
-      Enable_nosso = Ena.addAcceleration(AcZ);
-      if (digitalRead(10) == HIGH) {
-        Enable_eletronica = 1;
-      }
-    }
-
-    if (addr < EEPROM.length()) {
-      if ((micros_atual - micros_anterior_salvamento_de_dados) >= intervalo_de_salvamento_de_dados ) {
-        micros_anterior_salvamento_de_dados = micros_atual;
-        
-        ServoOutputSaved = 100 * ServoOutput; // double to int
-        dadoFiltradoSaved = 100 * dado_filtrado;        
-
-        writeIntEEPROM(addr, dado_filtrado); // Salva dado filtrado
-        addr = addr + 2; // move a pduas posições na memoria
-        writeIntEEPROM(addr, ServoOutput); // Salva output para Servo
-        addr = addr + 2;
-        writeIntEEPROM(addr, AcZ_raw);
-        addr = addr + 2;
-//        Serial.println("Saved EEPROM");
-      }
-    }
-
-    fim = micros(); // <-------------------
-//    Serial.print("Tempo lendo dados: "); // <-------------------
-//    Serial.println(fim - inicio); // <-------------------
-
-    inicio = micros(); // <-------------------
-
-    Serial.print("GyZ = "); Serial.print(GyZ);
-    Serial.print(" | GyZ filtrado = "); Serial.println(dado_filtrado);
-//    Serial.print(" | Servo Position = "); Serial.println(ServoOutput);
-
-    fim = micros(); // <-------------------
-//    Serial.print("Tempo gravando dados: "); // <-------------------
-//    Serial.println(fim - inicio); // <-------------------
-
-  }
-}
-
-// =================================================================================================================================
-void writeIntEEPROM(int address, int number)
-{
-  /* writes a int into the an addres in the eeprom by converting
-  */
-  byte byte1 = number >> 8;
-  byte byte2 = number & 0xFF;
-  EEPROM.write(address, byte1);
-  EEPROM.write(address + 1, byte2);
 }
