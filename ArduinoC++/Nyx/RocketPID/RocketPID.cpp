b347d9ce8caebde971728f65c16f67890c1a141e
Lucas Wu Jiajun <108297692+wucaslu@users.noreply.github.com>
2023-06-04 19:23:23 -0300

    Update camp and windup limits

--- ArduinoC++/Nyx/RocketPID/RocketPID.cpp
+++ ArduinoC++/Nyx/RocketPID/RocketPID.cpp
@@ -35,7 +35,7 @@ float RocketPID::computePID(double input){
 
     //Compute integrative and derivative terms
     integrative += Ki * err * dt;
-    integrative = clamp(integrative) * 180/3.1416;  //Avoid integrative windup
+    integrative = antiWindUp(integrative, lower * 180/3.1416, upper * 180/3.1416);  //Avoid integrative windup (-lowerInput, +upperInput)
 
     derivative = -Kd * d_input / dt;
 
@@ -50,7 +50,7 @@ return output;
 }
 
 void RocketPID::setLimits(float lowerInput, float upperInput){
-    lower = lowerInput * 3.1416/180;
+    lower = lowerInput * 3.1416/180; //Inputs are in degrees
     upper = upperInput * 3.1416/180;
 }
 
@@ -65,6 +65,15 @@ float RocketPID::clamp(float value){
         return lower;
 return value;
 }
+
+float RocketPID::antiWindUp(float integrative, float lowerInput, float upperInput){
+    if (integrative > upperInput)
+        return upperInput;
+    if (integrative < lowerInput)
+        return lowerInput;
+    return integrative;
+}
+
 
 float RocketPID::getSetPoint(){
     return setPoint;
